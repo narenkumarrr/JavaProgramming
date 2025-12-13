80b6660474acccaada0ca59cf28f4ba66783e671
NareN <kumarnaren0109@gmail.com>
2025-12-13 13:26:30 +0530

    v1

---
+++ Product.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Product {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        int b;
+        b=sc.nextInt();
+        System.out.println("Their product=>"+a*b);
+    }
+}
