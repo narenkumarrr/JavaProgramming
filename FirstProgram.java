661996b5eef1c8028c60cf918d373ed8f26e2e73
NareN <kumarnaren0109@gmail.com>
2025-12-13 12:35:31 +0530

    initial  commit

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+// import java.util.*;
+public class FirstProgram {
+    public static void main(String args[]){
+        int a=1;
+        int b=3;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
---
+++ ScanSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ScanSum {
+    public static void main(String args[]){
+        int a;
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+      
+        int b;
+        b= sc.nextInt();
+
+        sc.close();
+        int sum=a+b;
+        System.out.println(sum);
+
+
+
+    }
+}
